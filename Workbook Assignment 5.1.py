436671bba57e3d475a92433020c7e7c259cc707a
carolinaa18 <70179460+carolinaa18@users.noreply.github.com>
2020-10-06 22:49:53 -0400

    Add files via upload

---
+++ Workbook Assignment 5.1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+Location = "gradedata.csv"
+df = pd.read_csv(Location)
+df.take(np.random.permutation(len(df))[:500])
+
+
+# In[ ]:
+
+
+
+
